eff244778af84d45dbb0534d9ca7e0f797ebcb69
emanuel <emanuel123.calvovilla@gmail.com>
2024-04-07 00:57:07 -0500

    three

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 app.use('/api/v1', productosRouter)
+app.get('/', (req, res) => {
+    res.redirect('/api/v1/');
+})
 app.listen(port, () => {
     console.log('API RUNNING ON PORT 3000 🚀 YOU CAN SEE THE ROUTES IN localhost:3000/api/v1/productos')
 })
