21f87549c04ce629cabcd551dae60c1c130f36f5
emanuel <emanuel123.calvovilla@gmail.com>
2024-04-07 01:05:09 -0500

    three

--- database/config.js
+++ database/config.js
@@ -1,2 +1,2 @@
 import mongoose from 'mongoose'
-mongoose.connect("mongodb+srv://Emanuel474:calvo123@cluster0.koh1rzt.mongodb.net/").then(()=>console.log('Conectado a mongo atlas')).catch(e=>console.error(e))
+mongoose.connect("mongodb+srv://Emanuel474:calvo123@cluster0.koh1rzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log('Conectado a mongo atlas')).catch(e=>console.error(e))
