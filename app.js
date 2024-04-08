0671e79abe8fb296d0bee9a0ac87777697090eb3
emanuel <emanuel123.calvovilla@gmail.com>
2024-04-07 22:02:40 -0500

    s

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import productosRouter from './routes/productos.js';
 import './database/config.js'
+import cors from "cors"
 const port = 3000;
 const app = express();
 app.use(express.json());
+const whitelist = ['http://localhost:3000']; // assuming front-end application is running on localhost port 3000
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use('/api/v1', productosRouter)
 app.get('/', (req, res) => {
     res.redirect('/api/v1/');
@@ -11,3 +25,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('API RUNNING ON PORT 3000 🚀 YOU CAN SEE THE ROUTES IN localhost:3000/api/v1/productos')
 })
+
