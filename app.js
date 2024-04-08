db9e954f1572b4f61fe18428cc77f993037c4fe6
emanuel <emanuel123.calvovilla@gmail.com>
2024-04-07 22:14:23 -0500

    s

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 import express from 'express';
 import productosRouter from './routes/productos.js';
 import './database/config.js'
-import cors from "cors"
+import cors from 'cors'
 const port = 3000;
 const app = express();
 app.use(express.json());
-const whitelist = ['http://localhost:3000']; // assuming front-end application is running on localhost port 3000
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/api/v1', productosRouter)
 app.get('/', (req, res) => {
     res.redirect('/api/v1/');
